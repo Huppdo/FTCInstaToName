95c91ad826d28444e1eac405ab696c73f2cb0130
JibbySnip <endershadowz122@gmail.com>
2019-12-27 20:42:40 -0700

    Made everything but insta and google sheets

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This is a program intended to scrape names from an Instagram followers list and cross reference them with TOA data to get team numbers and names.
 
 Credit to:
-Finn - 16941 Wrench Toast
-Dom - 15692 Rust in Pieces
+\nFinn - 16941 Wrench Toast
+\nDominic - 15692 Rust in Pieces
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fuzzywuzzy import fuzz
 import requests
-from regex import sub
+from re import sub
 
 allTeams = []
 bad_words = ['robotics', 'robots','bots','robotic','bot','robot']
 PARAMS = {'Content-Type':'application/json',
-            'X-TOA-Key':'nice try mate',
+            'X-TOA-Key':'bf16312421adc97f496c678500a6c3a95ede30a0315ceced072f634861072e2d',
             'X-Application-Origin':'Atom'}
 
+debug = True
 
 def _get_number_and_name(name):
     return sub('[^0-9]','', name), sub('[^a-z]','',name) #Returns a tuple of the team number and name as strings, if present
@@ -15,30 +16,65 @@ def _get_number_and_name(name):
 
 
 def check_name_and_number(searchingName):
-    possible = ''
+    if debug: print("{0.BOLD}{1}{0.END}:".format(color,searchingName)) #I know, I know. All of this if debug nonsense just clutters up the code. Whatever.
+    possible = []
     found = False
-    number,name = _get_number_and_name(searchingName)
+    number, name = _get_number_and_name(searchingName)
     if name not in bad_words:
         for i in range(len(allTeams)):
+            if str(allTeams[i]['team_key']) == number:
+                possible.append((str(allTeams[i]['team_name_short']),str(allTeams[i]['team_key'])))
+                found = True
+                if debug: print("\tA team, {0} was found by their number {1}".format(allTeams[i]['team_name_short'],allTeams[i]['team_key']))
+                continue
             try:
                 if fuzz.ratio(allTeams[i]["team_name_short"].lower().replace(" ", ""),str(name)) >= 75: #Detects at least 75% similarity in the team names
-                    possible += ', ' + str(allTeams[i]["team_key"]) if len(possible) != 0 else str(allTeams[i]["team_key"]) #adds the found team to the list
+                    possible.append((str(allTeams[i]["team_name_short"]),str(allTeams[i]["team_key"]))) #adds the found team's name and number to the list of possible teams
                     found = True
+                    if debug: print("\tA team, {0} was found by their name".format(allTeams[i]['team_name_short']))
             except AttributeError:
                 continue
+
     else:
-        sendText(number, "That is an invalid search word. (EC3 - Overflow)")
-        return True
+        if debug: print("\tThe name {} is not valid".format(searchingName))
+        return None
     if found == False:
-        sendText(number, "That team name was not found. Please try again")
-    return possible
+        if debug: print("\tThe name {} was not found".format(searchingName))
+        return None
+    return (possible[0] if len(possible) == 1 else possible)
 
 def scrape_all_teams():
-    while True:
-        url = 'https://theorangealliance.org/api/team/'
-            data = requests.get(url,headers=PARAMS)
-            print('data is a ',type(data))
-            print('the len of data is ',len(data))
+    url = 'https://theorangealliance.org/api/team/'
+    return requests.get(url,headers=PARAMS).json()
 
-if __name__ == '__main__':
-    scrape_all_teams() #this is set up just for testing the scraping
+def run_bulk_teams(handles):
+    problems = []
+    teams = []
+    team_info = list(filter(None,list(map(check_name_and_number,handles)))) #Runs handles through check_name_and_number, and results in a list of tuples containing a team name and number.
+    for i in range(len(team_info)):
+        numbers = team_info[i]
+        if type(numbers) == list:
+            problems.append(team_info[i])
+            print(team_info[i],'was sent to problems')
+        elif type(numbers[0]) == str:
+            teams.append([numbers[0],numbers[1],handles[i]]) #A list. Field 1 is the name, 2 is the number, 3 is the handle.
+            print(team_info[i],'was sent to teams')
+    return problems,teams
+
+class color: #For fancy formatting for debug because I'm extra
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+if __name__ == '__main__': #yay it works
+    allTeams = scrape_all_teams()
+    problems, teams = run_bulk_teams()
+    print('problems',problems)
+    print('teams',teams)
